399ef1c6c881c28b48e0fc8514a7860f69c9f09c
BennyQBD <bennyqbdspam@gmail.com>
2018-06-26 16:32:45 -0400

    Updated for Tutorial 16

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,162 @@
 #include "core/input.hpp"
 #include "ecs/ecs.hpp"
 
+namespace MotionIntegrators
+{
+	void verlet(Vector3f& pos, Vector3f& velocity, const Vector3f& acceleration, float delta)
+	{
+		float halfDelta = delta * 0.5f;
+		pos += velocity * halfDelta;
+		velocity += acceleration * delta;
+		pos += velocity * halfDelta;
+	}
+
+	void forestRuth(Vector3f& pos, Vector3f& velocity, const Vector3f& acceleration, float delta)
+	{
+		static const float frCoefficient = 1.0f/(2.0f-Math::pow(2.0f,1.0f/3.0f));
+		static const float frComplement = 1.0f - 2.0f*frCoefficient;
+		verlet(pos, velocity, acceleration, delta*frCoefficient);
+		verlet(pos, velocity, acceleration, delta*frComplement);
+		verlet(pos, velocity, acceleration, delta*frCoefficient);
+	}
+
+	void modifiedEuler(Vector3f& pos, Vector3f& velocity, const Vector3f& acceleration, float delta)
+	{
+		velocity += acceleration * delta;
+		pos += velocity * delta;
+	}
+}
+
+struct TransformComponent : public ECSComponent<TransformComponent>
+{
+	Transform transform;
+};
+
+struct MovementControlComponent : public ECSComponent<MovementControlComponent>
+{
+	Array<std::pair<Vector3f, InputControl*> > movementControls;
+};
+
+struct RenderableMeshComponent : public ECSComponent<RenderableMeshComponent>
+{
+	VertexArray* vertexArray = nullptr;
+	Texture* texture = nullptr;
+};
+
+struct MotionComponent : public ECSComponent<MotionComponent>
+{
+	Vector3f velocity = Vector3f(0.0f, 0.0f, 0.0f);
+	Vector3f acceleration = Vector3f(0.0f, 0.0f, 0.0f);
+};
+
+class MotionSystem : public BaseECSSystem
+{
+public:
+	MotionSystem() : BaseECSSystem()
+	{
+		addComponentType(TransformComponent::ID);
+		addComponentType(MotionComponent::ID);
+	}
+
+	virtual void updateComponents(float delta, BaseECSComponent** components)
+	{
+		TransformComponent* transform = (TransformComponent*)components[0];
+		MotionComponent* motion = (MotionComponent*)components[1];
+
+		Vector3f newPos = transform->transform.getTranslation();
+		MotionIntegrators::forestRuth(newPos, motion->velocity, motion->acceleration, delta);
+		transform->transform.setTranslation(newPos);
+	}
+};
+
+class MovementControlSystem : public BaseECSSystem
+{
+public:
+	MovementControlSystem() : BaseECSSystem()
+	{
+		addComponentType(TransformComponent::ID);
+		addComponentType(MovementControlComponent::ID);
+	}
+
+	virtual void updateComponents(float delta, BaseECSComponent** components)
+	{
+		TransformComponent* transform = (TransformComponent*)components[0];
+		MovementControlComponent* movementControl = (MovementControlComponent*)components[1];
+
+		for(uint32 i = 0; i < movementControl->movementControls.size(); i++) {
+			Vector3f movement = movementControl->movementControls[i].first;
+			InputControl* input = movementControl->movementControls[i].second;
+
+			Vector3f newPos = transform->transform.getTranslation()
+				+ movement * input->getAmt() * delta;
+			transform->transform.setTranslation(newPos);
+		}
+	}
+};
+
+class GameRenderContext : public RenderContext
+{
+public:
+	GameRenderContext(RenderDevice& deviceIn, RenderTarget& targetIn, RenderDevice::DrawParams& drawParamsIn,
+			Shader& shaderIn, Sampler& samplerIn, const Matrix& perspectiveIn) : RenderContext(deviceIn, targetIn),
+		drawParams(drawParamsIn), shader(shaderIn), sampler(samplerIn), perspective(perspectiveIn) {}
+	
+	void renderMesh(VertexArray& vertexArray, Texture& texture, const Matrix& transformIn)
+	{
+		meshRenderBuffer[std::make_pair(&vertexArray, &texture)].push_back(perspective * transformIn);
+	}
+
+	void flush()
+	{
+		Texture* currentTexture = nullptr;
+		for(Map<std::pair<VertexArray*, Texture*>, Array<Matrix> >::iterator it
+				= meshRenderBuffer.begin(); it != meshRenderBuffer.end(); ++it) {
+			VertexArray* vertexArray = it->first.first;
+			Texture* texture = it->first.second;
+			Matrix* transforms = &it->second[0];
+			size_t numTransforms = it->second.size();
+
+			if(numTransforms == 0) {
+				continue;
+			}
+
+			if(texture != currentTexture) {
+				shader.setSampler("diffuse", *texture, sampler, 0);
+			}
+			vertexArray->updateBuffer(4, transforms, numTransforms*sizeof(Matrix));
+			this->draw(shader, *vertexArray, drawParams, numTransforms);
+			it->second.clear();
+		}
+	}
+private:
+	RenderDevice::DrawParams& drawParams;
+	Shader& shader;
+	Sampler& sampler;
+	Matrix perspective;
+	Map<std::pair<VertexArray*, Texture*>, Array<Matrix> > meshRenderBuffer;
+};
+
+class RenderableMeshSystem : public BaseECSSystem
+{
+public:
+	RenderableMeshSystem(GameRenderContext& contextIn) : BaseECSSystem(), context(contextIn)
+	{
+		addComponentType(TransformComponent::ID);
+		addComponentType(RenderableMeshComponent::ID);
+	}
+
+	virtual void updateComponents(float delta, BaseECSComponent** components)
+	{
+		TransformComponent* transform = (TransformComponent*)components[0];
+		RenderableMeshComponent* mesh = (RenderableMeshComponent*)components[1];
+
+		context.renderMesh(*mesh->vertexArray, *mesh->texture, transform->transform.toMatrix());
+	}
+private:
+	GameRenderContext& context;
+};
+
+
 // NOTE: Profiling reveals that in the current instanced rendering system:
 // - Updating the buffer takes more time than
 // - Calculating the transforms which takes more time than
@@ -29,13 +185,13 @@ static int runApp(Application* app)
 
 	// Begin scene creation
 	RenderDevice device(window);
-	RenderTarget target(device);
-	RenderContext context(device, target);
 
 	Array<IndexedModel> models;
 	Array<uint32> modelMaterialIndices;
 	Array<MaterialSpec> modelMaterials;
 	ModelLoader::loadModels("./res/models/monkey3.obj", models,
+			modelMaterialIndices, modelMaterials);
+	ModelLoader::loadModels("./res/models/tinycube.obj", models,
 			modelMaterialIndices, modelMaterials);
 //	IndexedModel model;
 //	model.allocateElement(3); // Positions
@@ -54,6 +210,7 @@ static int runApp(Application* app)
 //	model.addIndices3i(0, 1, 2);
 
 	VertexArray vertexArray(device, models[0], RenderDevice::USAGE_STATIC_DRAW);
+	VertexArray tinyCubeVertexArray(device, models[1], RenderDevice::USAGE_STATIC_DRAW);
 	Sampler sampler(device, RenderDevice::FILTER_LINEAR_MIPMAP_LINEAR);
 //	ArrayBitmap bitmap;
 //	bitmap.set(0,0, Color::WHITE.toInt());
@@ -68,7 +225,12 @@ static int runApp(Application* app)
 		return 1;
 	}
 	Texture texture(device, ddsTexture);
-
+	if(!ddsTexture.load("./res/textures/bricks2.dds")) {
+		DEBUG_LOG("Main", LOG_ERROR, "Could not load texture!");
+		return 1;
+	}
+	Texture bricks2Texture(device, ddsTexture);
+	
 	String shaderText;
 	StringFuncs::loadTextFileWithIncludes(shaderText, "./res/shaders/basicShader.glsl", "#include");
 	Shader shader(device, shaderText);
@@ -76,26 +238,7 @@ static int runApp(Application* app)
 	
 	Matrix perspective(Matrix::perspective(Math::toRadians(70.0f/2.0f),
 				4.0f/3.0f, 0.1f, 1000.0f));
-	float amt = 0.0f;
 	Color color(0.0f, 0.15f, 0.3f);
-	float randZ = 20.0f;
-	float randScaleX = randZ * window.getWidth()/(float)window.getHeight();
-	float randScaleY = randZ;
-	
-	uint32 numInstances = 1;
-	Matrix transformMatrix(Matrix::identity());
-	Transform transform;
-	Array<Matrix> transformMatrixArray;
-	Array<Matrix> transformMatrixBaseArray;
-	for(uint32 i = 0; i < numInstances; i++) {
-		transformMatrixArray.push_back(Matrix::identity());
-		transform.setTranslation(Vector3f(0.0f, 0.0f,
-//					(Math::randf() * randScaleX)-randScaleX/2.0f,
-//					(Math::randf() * randScaleY)-randScaleY/2.0f,
-					randZ));
-		transformMatrixBaseArray.push_back(transform.toMatrix());
-	}
-	transform.setTranslation(Vector3f(0.0f,0.0f,0.0f));
 	
 	RenderDevice::DrawParams drawParams;
 	drawParams.primitiveType = RenderDevice::PRIMITIVE_TRIANGLES;
@@ -106,6 +249,8 @@ static int runApp(Application* app)
 //	drawParams.destBlend = RenderDevice::BLEND_FUNC_ONE;
 	// End scene creation
 
+	RenderTarget target(device);
+	GameRenderContext gameRenderContext(device, target, drawParams, shader, sampler, perspective);
 	GameEventHandler eventHandler;
 	InputControl horizontal;
 	InputControl vertical;
@@ -118,8 +263,44 @@ static int runApp(Application* app)
 	eventHandler.addKeyControl(Input::KEY_UP, vertical, 1.0f);
 	eventHandler.addKeyControl(Input::KEY_DOWN, vertical, -1.0f);
 
-	float xPos = 0.0f;
-	float yPos = 0.0f;
+	ECS ecs;
+	// Create components
+	TransformComponent transformComponent;
+	transformComponent.transform.setTranslation(Vector3f(0.0f, 0.0f, 20.0f));
+
+	MovementControlComponent movementControl;
+	movementControl.movementControls.push_back(std::make_pair(Vector3f(1.0f,0.0f,0.0f) * 10.0f, &horizontal));
+	movementControl.movementControls.push_back(std::make_pair(Vector3f(0.0f,1.0f,0.0f) * 10.0f, &vertical));
+
+	RenderableMeshComponent renderableMesh;
+	renderableMesh.vertexArray = &vertexArray;
+	renderableMesh.texture = &texture;
+
+	MotionComponent motionComponent;
+	// Create entities
+	ecs.makeEntity(transformComponent, movementControl, renderableMesh);
+	for(uint32 i = 0; i < 5000; i++) {
+		transformComponent.transform.setTranslation(Vector3f(Math::randf()*10.0f-5.0f, Math::randf()*10.0f-5.0f, 
+					Math::randf()*10.0f-5.0f + 20.0f));
+		renderableMesh.vertexArray = &tinyCubeVertexArray;
+		renderableMesh.texture = Math::randf() > 0.5f ? &texture : &bricks2Texture;
+		
+		float vf = -4.0f;
+		float af = 5.0f;
+		motionComponent.acceleration = Vector3f(Math::randf(-af, af), Math::randf(-af, af), Math::randf(-af, af));
+		motionComponent.velocity = motionComponent.acceleration * vf;
+		ecs.makeEntity(transformComponent, motionComponent, renderableMesh);
+	}
+	
+	// Create the systems
+	MovementControlSystem movementControlSystem;
+	MotionSystem motionSystem;
+	RenderableMeshSystem renderableMeshSystem(gameRenderContext);
+	ECSSystemList mainSystems;
+	ECSSystemList renderingPipeline;
+	mainSystems.addSystem(movementControlSystem);
+	mainSystems.addSystem(motionSystem);
+	renderingPipeline.addSystem(renderableMeshSystem);
 
 	uint32 fps = 0;
 	double lastTime = Time::getTime();
@@ -145,16 +326,7 @@ static int runApp(Application* app)
 		while(updateTimer >= frameTime) {
 			app->processMessages(frameTime, eventHandler);
 			// Begin scene update
-			xPos += 10.0f*frameTime*horizontal.getAmt();
-			yPos += 10.0f*frameTime*vertical.getAmt();
-			transform.setRotation(Quaternion(Vector3f(1.0f, 1.0f, 1.0f).normalized(), amt*10.0f/11.0f));
-			transform.setTranslation(Vector3f(xPos, yPos, 0.0f));
-			for(uint32 i = 0; i < transformMatrixArray.size(); i++) {
-				transformMatrixArray[i] = (perspective * transformMatrixBaseArray[i] * transform.toMatrix());
-			}
-			vertexArray.updateBuffer(4, &transformMatrixArray[0],
-					transformMatrixArray.size() * sizeof(Matrix));
-			amt += (float)frameTime/2.0f;
+			ecs.updateSystems(mainSystems, frameTime);
 			// End scene update
 
 			updateTimer -= frameTime;
@@ -163,8 +335,9 @@ static int runApp(Application* app)
 		
 		if(shouldRender) {
 			// Begin scene render
-			context.clear(color, true);
-			context.draw(shader, vertexArray, drawParams, numInstances);
+			gameRenderContext.clear(color, true);
+			ecs.updateSystems(renderingPipeline, frameTime);
+			gameRenderContext.flush();
 			// End scene render
 			
 			window.present();
